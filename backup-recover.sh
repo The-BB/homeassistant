959502341afcb14fff2355fb8fb68b6f64a74753
The-BB <tun.chen.bo@gmail.com>
2023-08-06 13:19:27 +0300

    backup-recover.sh: update

--- backup-recover.sh
+++ backup-recover.sh
@@ -9,6 +9,7 @@ PATCH_DIR="./feeds/homeassistant/_patches"
 PATCH_PACKAGES="\
 $PATCH_DIR/lang-python-Jinja2-bump-to-3.1.2.patch
 $PATCH_DIR/lang-python-numpy-bump-to-1.24.3.patch
+$PATCH_DIR/lang-python-pillow-bump-to-10.0.0.patch
 $PATCH_DIR/lang-python-python-aiohttp-bump-to-3.8.5.patch
 $PATCH_DIR/lang-python-python-async-timeout-bump-to-4.0.2.patch
 $PATCH_DIR/lang-python-python-awesomeversion-bump-to-22.9.0.patch
@@ -16,7 +17,7 @@ $PATCH_DIR/lang-python-python-bleak-bump-to-0.20.2.patch
 $PATCH_DIR/lang-python-python-boto3-bump-to-1.16.52.patch
 $PATCH_DIR/lang-python-python-botocore-bump-to-1.19.52.patch
 $PATCH_DIR/lang-python-python-ciso8601-bump-to-2.3.0.patch
-$PATCH_DIR/lang-python-python-dbus-fast-bump-to-1.86.0.patch
+$PATCH_DIR/lang-python-python-dbus-fast-bump-to-1.87.5.patch
 $PATCH_DIR/lang-python-python-greenlet-bump-to-2.0.2.patch
 $PATCH_DIR/lang-python-python-ifaddr-bump-to-0.2.0.patch
 $PATCH_DIR/lang-python-python-multidict-bump-to-6.0.2.patch
@@ -28,14 +29,14 @@ $PATCH_DIR/lang-python-python-pyserial-bump-to-3.5.patch
 $PATCH_DIR/lang-python-python-requests-bump-to-2.31.0.patch
 $PATCH_DIR/lang-python-python-s3transfer-bump-to-0.3.3.patch
 $PATCH_DIR/lang-python-python-sqlalchemy-bump-to-2.0.15.patch
-$PATCH_DIR/lang-python-python-typing-extensions-bump-to-4.6.3.patch
+$PATCH_DIR/lang-python-python-typing-extensions-bump-to-4.7.0.patch
 $PATCH_DIR/lang-python-python-urllib3-bump-to-1.26.5.patch
 $PATCH_DIR/lang-python-python-voluptuous-bump-to-0.13.1.patch
 $PATCH_DIR/lang-python-python-voluptuous-serialize-bump-to-2.6.0.patch
 $PATCH_DIR/lang-python-python-websockets-bump-to-11.0.1.patch
 $PATCH_DIR/lang-python-python-yaml-bump-to-6.0.1.patch
 $PATCH_DIR/lang-python-python-yarl-bump-to-1.9.2.patch
-$PATCH_DIR/lang-python-python-zeroconf-bump-to-0.70.0.patch
+$PATCH_DIR/lang-python-python-zeroconf-bump-to-0.72.0.patch
 $PATCH_DIR/lang-python-python3-netifaces-bump-to-0.11.0.patch
 $PATCH_DIR/lang-python-python3-pyroute2-bump-to-0.7.5.patch
 $PATCH_DIR/lang-python-xmltodict-bump-to-0.13.0.patch
